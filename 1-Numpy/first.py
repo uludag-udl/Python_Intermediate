ae1042433cf923384624951e9f20a3f082e06f31
Uludağ Yapay Zeka Topluluğu <56990250+uludag-udl@users.noreply.github.com>
2019-11-16 23:23:29 +0300

    Update first.py

--- 1-Numpy/first.py
+++ 1-Numpy/first.py
@@ -1,1 +1,31 @@
-import numpy as np
+1-) numpy kütüphanesini import ediniz
+
+2-) 1'den 100'e kadar (100 dahil) sayıları bir x dizisinde gösterin
+
+3) 
+   a) 2'den 30'e kadar 3'er artan sayıları y dizisinde gösterin
+   b)y isimli dizinizi 2 satır 5 sütunluk bir matris halinde gösterin
+   c)y dizisindeki en büyük,en küçük elmanı gösterin
+   d)y dizisinin elemanları toplamını gösterin
+   e)y dizisinin satır ve sütun toplamlarını gösterin
+   f)y dizisinin ortalamasını gösterin
+    
+  
+4) x dizisi ve y matrisinin boyutlarını gösteriniz
+5)y matrisini ters çeviriniz
+
+5) 
+a)içinde yalnızca u d l harfleri bulunan bir k dizisi 
+b)içinde 2 0 1 8 rakamları bulunan bir m dizisi
+c)içinde u d l 2 0 1 8 harf ve rakamlarını birlikte bulunduran biz n dizisi oluşturun
+bu 3 dizi elemanlarının veri tipini gösterip yorumlayın
+
+6)30 adet 0'dan oluşan 5*6 lık bir dizi oluşturun
+
+7)3x5 boyutlarında 10 ile 50 arasında rastgele bir sayı üretip sayi isimli değişkene atayınız
+  
+
+
+
+
+
